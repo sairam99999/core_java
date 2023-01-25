9e0e2b9366bcd21a8ddd1f28317dc92336eb0665
sairam99999 <sairampenumuchhu99@gmail.com>
2023-01-25 11:40:54 -0600

    made a change to the add class file

--- Add.java
+++ Add.java
@@ -6,8 +6,12 @@ public class Add{
     public static void main(String[] args)
     {
       System.out.println("hi");
+      System.out.println("hello");
       System.out.println("hello iam sairam");
-      System.out.println("hello iam sairam");
+      int x=10;
+      int y=12;
+      int z=x*y;
+      System.out.pritnln(z);
         
     }
 
